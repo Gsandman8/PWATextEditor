611659dc5d72b36abc3d970668be5cda92cff77a
Gsandman8 <gage.e.sandman@gmail.com>
2024-01-29 14:10:52 -0800

    Added webpack configurations to set up service worker, manifest and CSS loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,53 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        filename: 'index.html',
+        title : "JATE"
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'JATE Text Editor',
+        short_name: 'JATE',
+        description: 'A simple text editor utlizing webpack and IDB wrapper',
+        background_color: '#ffffff',
+        theme_color: '#aaaaaa',
+        start_url: '/',
+        public_path: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+    ],
+      }),
     ],
 
     module: {
-      rules: [
-        
+      rules: [ {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { 
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-runtime']},
+        },
+      }
       ],
     },
   };
