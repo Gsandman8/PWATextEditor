40f7b4e63b9f20452afac304b7a9009dd12b35f2
Gsandman8 <gage.e.sandman@gmail.com>
2024-01-29 15:18:08 -0800

    Added gitignores to server and root

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -62,7 +60,7 @@ module.exports = () => {
           loader: 'babel-loader',
           options: { 
             presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-runtime']},
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime']},
         },
       }
       ],
